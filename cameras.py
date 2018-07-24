4c0086cc6bb744eec85c53b686bb676fc1301a2f
Becki <becki@beckitrue.com>
2018-07-24 09:21:02 -0500

    added filename input, csv output, eliminated duplicate mac entries

--- cameras.py
+++ cameras.py
@@ -1,5 +1,5 @@
 # We want to map the camera MAC to IP using data collected from Wireshark
-# 
+
 # Wireshark filter is: ip.dst == 255.255.255.255 and udp.port == 8848 
 # Port 8848 UDP is used by MESSOA IP cameras as a heartbeat. Every few 
 # seconds they send a small comma separated string: 
@@ -10,54 +10,80 @@
 # for IP to MAC mapping
 
 # Output 
-# CSV MAC, IP, subnetmask, port 
+# CSV: MAC, IP
 
 # --------------------
-# stuff to do to make this better - right now I fix it in Excel
 
-# ask for location of data file - need to fix this to be interactive
-#data_file = input ('path to file: ')
-#print ('data file is: '+ data_file)
-
-# need to get location of file to write to 
-
-# need to put records in an indexed list or something index by MAC
-# ----------------------
-
-# open file and parrse it to get the data we need
-
-# we're going to use regex
 import re
 import os
+import sys
+import csv
 
-def append_data(line_num, data_line):
+def create_list(line_num, data_line):
     "gets data information to write record to a file"
     # INPUT
     # data line number i.e. 0010, 0020, 0030, 0040
     # information to append to record 
+    # we're using a dictionary data structure to avoid duplicate records
+    # and we'll use the MAC as the key in the key:value pair
 
-    # if line is 0010, this is a new record so send a new line to file
+
+    global record
+    global macpat
+    global ippat
+    global cameras
+    
     if line_num == "0010":
-        record = "\n"+data_line
-    else: 
+    # this is a new data record of a camera
+    # start new string record of the camera details
         record = data_line
-    print (record)
-    # open the file to write to
-    fmap.write(record)
+    else: 
+        # append to the string
+        record = record + data_line
+
+    if line_num == "0030":
+    # This is the last data line with information we need -
+    # find the MAC so we can see if this is a duplicate device - 
+    # if not, add it to our list of devices and map its IP to it
+
+        # look for mac address in record
+        mac = macpat.search(record)
+        if mac:
+            mac_add = mac.group(0)
+            # find the IP address in the record
+            ip = ippat.search(record)
+            ip_add = ip.group(0)
+
+            # see if the MAC address is already in our list of devices
+            # add it if not
+            if mac_add not in cameras:
+                cameras[mac_add] = ip_add
     return
 
+# open pcap text file and parrse it to get the data we need
 # look for the lines with the data
 # data lines start with 4 digits followed by a space and then up to 16 hex pairs
 # there are never more tha 5 lines of data in these messages: 
 # 0000, 0010, 0020, 0030, 0040
 # line 0000 always has the string "$MessoaIPCamera," so might as well skip it
-p = re.compile(r'(00[1-4]0)\s+')
+# line 0040 always has the TCP/UDP port, so might as well skip that too
+p = re.compile(r'(00[1-3]0)\s+')
+# pattern for MAC
+macpat = re.compile(r'([0-9a-fA-F]{2}:?){6}')
+# pattern for IPv4
+ippat = re.compile(r'([0-9]{1,3}\.?){4}')
+# initialize record string to an empty string
+record = ""
+# initialize cameras dictionary to an empty dictionary
+cameras = dict()
 
-# open the file with the pcap in plain text
-with open('/Users/becki/Desktop/pcaps/mapping.txt', 'a') as fmap, open('/Users/becki/Desktop/pcaps/cameras1', 'r') as f:
+# open the file to read with the pcap in plain text
+with open(input("Enter file to read: "),'r') as f:
     line = f.readline()
     while line:
+        # look for data lines
         m = p.match(line)
+        # parse data lines to get the information we want
         if m:
             # remove trailing \n from all data lines
             data_line = re.sub(r'\n',"",line)
@@ -65,7 +91,14 @@ with open('/Users/becki/Desktop/pcaps/mapping.txt', 'a') as fmap, open('/Users/b
             data_line = re.sub(r'(00[1-4]0)\s+((\w){2}(\s?))+',"",data_line)
             # remove all spaces in the line
             data_line = data_line.replace(" ", "")
-            append_data(m.group(1), data_line)
+            # sent line number and parsed data to add to list
+            create_list(m.group(1), data_line)
         line = f.readline()
-f.closed()
-fmap.closed()
+# write device list to a CSV file
+with open(input("Enter filename to write: "),'w') as csvfile:
+    devices = csv.writer(csvfile)
+    # add header row
+    devices.writerow(["MAC", "IP"])
+    for key, val in cameras.items():
+        devices.writerow([key, val]) 
+
