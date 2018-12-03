fce33c08e579091907321b766f218eb337e2042f
Becki <becki@beckitrue.com>
2018-12-03 16:49:55 -0600

    updated formatting with Flake8

--- project_a.py
+++ project_a.py
@@ -2,10 +2,11 @@
 
 import random
 
-def guessing ():
-# Guess a randomly generated number between 1 and 99
+
+def guessing():
+    # Guess a randomly generated number between 1 and 99
     # generate a random number
-    n = random.randint(1,99)
+    n = random.randint(1, 99)
     # add a guess counter
     count = 1
     # the guessing loop logic
@@ -13,34 +14,36 @@ def guessing ():
     while n != guess:
         count += 1
         if guess > n:
-            print ("High. Try again: ")
+            print("High. Try again: ")
         else:
-            print ("Low. Try again: ")
+            print("Low. Try again: ")
         guess = int(input("Guess a number between 1 and 99: "))    
-    print ("Correct!! It took you {:d} guesses".format(count))
+    print("Correct!! It took you {:d} guesses".format(count))
     # see if they want to play again
     again = input("Would you like to play again? ")
     if (again == 'yes') or (again == 'y'):
         guessing()
     else:
-        print ("Thanks for playing")
+        print("Thanks for playing")
         return main()
 
+
 def diceroll():
-# dice rolling game with 2 dice
-    die_1 = random.randint(1,6)
-    die_2 = random.randint(1,6)
-    print ("your dice came up {:d} and {:d}".format(die_1, die_2))
+    # dice rolling game with 2 dice
+    die_1 = random.randint(1, 6)
+    die_2 = random.randint(1, 6)
+    print("your dice came up {:d} and {:d}".format(die_1, die_2))
     again = input("Would you like to play again? ")
     if (again == 'yes') or (again == 'y'):
         diceroll()
     else:
-        print ("Thanks for playing")
+        print("Thanks for playing")
         return main()
+
 
 def main():
     # ask the player to choose a game
-    print ("Let's play a game")
+    print("Let's play a game")
     game = int(input('''
         Pick a game.
 
@@ -54,13 +57,12 @@ def main():
     elif game == 2:
         diceroll()
     elif game == 0:
-        print ("Thanks for playing\n")
+        print("Thanks for playing\n")
         return
     else:
-        print ("Your input wasn't valid\n")
+        print("Your input wasn't valid\n")
         main()
     return
 
+
 main()
-
-
